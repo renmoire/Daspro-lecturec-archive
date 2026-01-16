945fc1638286c6b9ba8be0f0835b1e2cca318e87
Ren <214003478+renmoire@users.noreply.github.com>
2026-01-16 21:44:34 +0700

    Execute folders

--- Pertemuan 2/Latihan Daspro.py
+++
@@ -1,9 +0,0 @@
-# Nama: Irenia Maisa Kamila
-# NIM: 2506031
-# Kelas: 1B
-
-
-nama = input("Masukan nama Anda: ")
-tahunlahir = int(input("Masukan tahun lahirmu: "))
-tahunnow = 2025
-print(f"Halo, {nama}. Umurmu adalah {tahunnow - tahunlahir}, lahir di tahun {tahunlahir}.")
--- Pertemuan 7/Latihan 1.py
+++
@@ -1,9 +0,0 @@
-
-#! Nama: Irenia Maisa Kamila
-#! NIM: 2506031
-#! Kelas: 1B
-
-
-for k5 in range(1, 51):
-    if k5 % 5 == 0:
-        print(k5)
